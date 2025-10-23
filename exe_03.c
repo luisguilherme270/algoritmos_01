f88f69a22fc22dac495628dee00db8a59cdc8343
Luis Guilherme Viana <luis.camargo2@estudante.ifms.edu.br>
2025-10-23 13:11:29 -0400

    Update exe_03.c

--- exe_03.c
+++ exe_03.c
@@ -18,12 +18,9 @@ switch(escolha)
     }
   case 2:
     {
-    int numero_a;
-    int numero_b;
-    int numero_c;
-    numero_a = 3;
-    numero_b = 10;
-    numero_c = 200;
+    int numero_a = 3;
+    int numero_b = 10;
+    int numero_c = 200;
     
     printf("%d %d %d\n", numero_a, numero_b, numero_c);
     break;
